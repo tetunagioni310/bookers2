bd362e97de17a2232d2e21b0e07ef63d3e8a857d
tetunagioni310 <104610603+tetunagioni310@users.noreply.github.com>
2022-08-18 14:36:06 +0000

    [Update]index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,37 @@
 class BooksController < ApplicationController
+
+ def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def destroy
+  end
+
   def index
+    @book = Book.new
     @books = Book.all
   end
-  
+
   def show
+    @book = Book.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
-  def create
-  end
-  
-  def destroy
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,29 @@
 class UsersController < ApplicationController
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
-    @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to '/users'
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name,:image)
+    params.require(:user).permit(:name,:image,:introduction)
   end
 
 end
